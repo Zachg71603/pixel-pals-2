8772399473d68bd7dee96e7e66c02350c822593f
Zach Gutierrez <zg71603@gmail.com>
2024-01-25 17:45:13 -0800

    committing second half

--- server.js
+++ server.js
@@ -1,71 +1,42 @@
+const firebase = require('firebase/app');
+const auth = require('firebase/auth');
 const cookieParser = require("cookie-parser");
-const csrf = require("csurf");
 const bodyParser = require("body-parser");
 const express = require("express");
-const exphbs = require('express-handlebars'); 
-const hbs = exphbs.create({ defaultLayout: 'main' });
-const admin = require("firebase-admin");
-// const ejs = require('ejs');
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ defaultLayout: "main" });
 
-const serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://pixel-pals-fdb7e-default-rtdb.firebaseio.com",
-});
-
-// Create a CSRF middleware that will add a CSRF token to all requests
-const csrfMiddleware = csrf({ cookie: true });
-
+const app = express();
 // Create an express application with port number from environment variable or default 3001
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// use ejs as the view engine
-// app.engine("html", require("ejs").renderFile);
+// initialize firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCiZF1VwaJ8d18jp6nJtC2AQnboz1AerYg",
+  authDomain: "pixel-pals-fdb7e.firebaseapp.com",
+  databaseURL: "https://pixel-pals-fdb7e-default-rtdb.firebaseio.com",
+  projectId: "pixel-pals-fdb7e",
+  storageBucket: "pixel-pals-fdb7e.appspot.com",
+  messagingSenderId: "308630756272",
+  appId: "1:308630756272:web:1f7f9e0703c0803b449475",
+  measurementId: "G-X42F0TX02X"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 // Configure Handlebars as the view engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 // serve the files out of ./public as our main files
 app.use(express.static("public"));
-
 // use the body parser middleware to parse the body of incoming requests
 app.use(bodyParser.json());
 // use the cookie parser middleware to parse cookies
 app.use(cookieParser());
-// use the CSRF middleware to add CSRF tokens to requests
-app.use(csrfMiddleware);
-
-// routing middleware that directs all requests to the appropriate route
-// middleware to generate cookie with XSRF token
-// next() is called to continue to the next middleware
-app.all("*", (req, res, next) => {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  next();
-});
-// // login route that renders the login page
-// app.get("/login", (req, res) => {
-//   res.render("login.html");
-// });
-// // signup route that renders the signup page
-// app.get("/signup", (req, res) => {
-//   res.render("signup.html");
-// });
-// // profile route that renders the profile page
-// app.get("/profile", (req, res) => {
-//   res.render("profile.html");
-// });
-// index route that renders the index.html page
-// app.get("/", (req, res) => {
-//   res.render("index.html");
-// });
-app.get('/', (req, res) => {
-  res.render('feed'); 
-});
+// use routes to serve window locations
+const routes = require("./routes/index");
+app.use('/', routes)
 // partial registration
 hbs.handlebars.registerPartial('header', './views/partials/header.handlebars');
-hbs.handlebars.registerPartial('footer', './views/partials/sidebar.handlebars');
-hbs.handlebars.registerPartial('createPost', './views/partials/createPost.handlebars');
+hbs.handlebars.registerPartial('sidebar', './views/partials/sidebar.handlebars');
 // start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
